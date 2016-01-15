c11d0701533f5c3168cb3e93bad84edf3d196483
Peter Hartree <web@peterhartree.co.uk>
2016-01-15 13:00:12 +0000

    Archive route should return array of data, not html
    Signed-off-by: Peter Hartree <web@peterhartree.co.uk>

--- custom-routes/archive.php
+++ custom-routes/archive.php
@@ -4,27 +4,74 @@
  *
  * @param array $data Options for the function.
  * @return string|null Post title for the latest,  * or null if none.
+ *   year => months => post names
  */
 function ngwp_get_archive( $data ) {
-
+    global $wpdb;
+    $archive = array();
     $args = array();
-    $args['echo'] = 0;
-
-    if(isset($data['type'])):
-        $args['type'] = $data['type'];
-    endif;
+    $args['numberposts'] = -1;
 
     if(isset($data['post_type'])):
         $args['post_type'] = $data['post_type'];
+    else:
+        $args['post_type'] = 'post';
     endif;
 
-    $url = get_bloginfo('url');
+    // Get all published posts (for use later)
+    $posts = get_posts( $args );
 
-    $archives = wp_get_archives( $args );
+    // Get years that have posts
+    $years_query = "SELECT YEAR(post_date) AS year FROM ". $wpdb->prefix ."posts WHERE post_type = '" . $args['post_type'] . "' AND post_status = 'publish' GROUP BY year DESC";
 
-    $archive = str_replace($url, '', $archives);
+    $years = $wpdb->get_results( $years_query );
 
-    return $archives;
+    // For each year, get months with posts
+    foreach ( $years as $year ):
+        $months_query = "SELECT MONTH(post_date) AS month FROM ". $wpdb->prefix ."posts WHERE post_type = '" . $args['post_type'] . "' AND post_status = 'publish' GROUP BY month DESC";
+
+        $months = $wpdb->get_results( $months_query );
+
+        // For each month with posts, get post titles and permalink
+        foreach ( $months as $key => $month ):
+            $month->posts = array();
+
+            $dateObj   = DateTime::createFromFormat('!m', $month->month);
+            $month->name = $dateObj->format('F');
+
+            foreach ( $posts as $post ):
+
+                $post_timestamp = strtotime($post->post_date);
+                $post_month = date( 'n' , $post_timestamp);
+                $post_year = date( 'Y' , $post_timestamp);
+
+                if($post_month === $month->month && $post_year == $year->year):
+                    $post_data = array();
+                    $post_data['post_title'] = $post->post_title;
+
+                    $post_args = array();
+                    $post_args['id'] = $post->ID;
+                    $post_args['type'] = $post->post_type;
+                    $post_args['slug'] = $post->post_name;
+                    $post_data['link'] = ngwp_get_url($post_args);
+
+                    $post_data['date_published'] = ngwp_get_published_date($post_args);
+
+                    $month->posts[] = $post_data;
+                endif;
+            endforeach;
+
+            if(count($month->posts) === 0):
+                unset($months[$key]);
+            endif;
+
+        endforeach;
+
+        $year->months = $months;
+    endforeach;
+
+    $archive = $years;
+    return $archive;
 }
 
 add_action( 'rest_api_init', function () {
